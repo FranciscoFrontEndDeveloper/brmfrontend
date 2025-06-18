71daff15a5e37dd82164a966dd86377a72f9c633
FRANCISCO <pachonetworking@gmail.com>
2025-06-18 12:51:07 -0500

    The information fills the table successfully

--- src/app/components/apirestinfo/apirestinfo.component.ts
+++ src/app/components/apirestinfo/apirestinfo.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import {
   apiInfo,
   ApidetailsService,
 } from './../../services/apidetails.service';
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -11,12 +14,10 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-
-
 @Component({
   selector: 'app-apirestinfo',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, CommonModule],
   templateUrl: './apirestinfo.component.html',
   styleUrl: './apirestinfo.component.sass',
 })
@@ -28,22 +29,25 @@ export class ApirestinfoComponent {
     //Add 'implements OnInit' to the class.
     this.apidetailsService.getApiInfo().subscribe((data) => {
       this.apiInfoGet = data;
+      this.dataSource.data = data
       console.log(this.apiInfoGet);
+      this.displayedColumns = Object.keys(data[0])
+      console.log(this.displayedColumns)
     });
   }
 
   public ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
 
   displayedColumns: string[] = [
     'position',
@@ -52,6 +56,6 @@ export class ApirestinfoComponent {
     'symbol',
     'details',
   ];
-  dataSource = this.ELEMENT_DATA;
+  dataSource = new MatTableDataSource<any>();
   clickedRows = new Set<PeriodicElement>();
 }
