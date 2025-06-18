4bdfa7635ab541911cb4c0ac4bce0416441caae0
FRANCISCO <pachonetworking@gmail.com>
2025-06-18 15:43:24 -0500

    table with only four header

--- src/app/components/apirestinfo/apirestinfo.component.ts
+++ src/app/components/apirestinfo/apirestinfo.component.ts
@@ -31,30 +31,15 @@ export class ApirestinfoComponent {
       this.apiInfoGet = data;
       this.dataSource.data = data
       console.log(this.apiInfoGet);
-      this.displayedColumns = Object.keys(data[0])
+      this.displayedColumns = Object.keys(data[0]).splice(0,4)
       console.log(this.displayedColumns)
     });
   }
 
-  public ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ];
+
 
   displayedColumns: string[] = [
-    'position',
-    'name',
-    'weight',
-    'symbol',
-    'details',
+
   ];
   dataSource = new MatTableDataSource<any>();
   clickedRows = new Set<PeriodicElement>();
